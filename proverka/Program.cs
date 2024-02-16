4e19537b8612f14bb80cde416f687aac49fae72b
ventsislav67 <vencimkolev@gmail.com>
2024-02-16 10:32:21 +0200

    add program!

---
+++ proverka/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("въведете години:");
+int m = int.Parse(Console.ReadLine());
+
+if (m > 18) 
+{
+    Console.WriteLine("Пълнолетен!");
+}
+  else 
+{
+    Console.WriteLine("Непълнолетен!");
+}
